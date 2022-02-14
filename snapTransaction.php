8a5ee3ca1c0b4352870b4c12a268b0d1da6ed0ac
Yazky Maulana <yazki.maulana@gmail.com>
2022-02-14 15:14:09 +0700

    Feat(Midtrans payment using Snap Payment)

---
+++ snapTransaction.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'vendor/autoload.php';
+
+// Set your Merchant Server Key
+\Midtrans\Config::$serverKey = 'SB-Mid-server-5mpCA2qdvy9vAAGJl1bHIQGr';
+// Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+\Midtrans\Config::$isProduction = false;
+// Set sanitization on (default)
+\Midtrans\Config::$isSanitized = true;
+// Set 3DS transaction for credit card to true
+\Midtrans\Config::$is3ds = true;
+ 
+$params = array(
+    'transaction_details' => array(
+        'order_id' => rand(),
+        'gross_amount' => 10000,
+    ),
+    'customer_details' => array(
+        'first_name' => 'Yazky',
+        'last_name' => 'Maulana',
+        'email' => 'yazz.maul@example.com',
+        'phone' => '08111222333',
+    ),
+);
+ 
+$snapToken = \Midtrans\Snap::getSnapToken($params);
+echo $snapToken;
